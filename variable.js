506ea0d8c507f3e44944fef17630abe01bf5a789
sejawal <sejawal77@gmail.com>
2024-03-02 09:22:24 +0500

    math expression

---
+++ variable.js
@@ -0,0 +1,210 @@
+//         chapter no 4
+//  variables names legal and illegal
+
+// no 1 
+//  declare 3 variables in one statement 
+ 
+// var  vehicleName = "rickshaw";
+// var  driver      = "salman";
+// var  vehicleColor = "grey"
+
+//  alert(driver + " " + "has" + " " + vehicleColor + " " + vehicleName);
+
+//  no 2 
+// declare 5 legal and illegal variables names 
+
+// = 5 legal variables names 
+
+// var  user ;
+// var userName;
+// var $user;
+// var user_name;
+// var user13;
+
+
+// 5 illegal varibles names 
+
+// var 1User;
+// var user-name;
+// var @username;
+// var -user;
+// var user@name;
+
+
+
+
+
+
+// no 3 
+// display this in your browser 
+
+
+
+// document.write("<h2> Rules for naming Js variables </h2>");
+
+// document.write("<p>  Variables names can only contain numbers , $ , and _  </p>");
+
+// document.write ("<p> for example $my_1stVariable </p> ")
+//  document.write(" <p>Variable must begin with a leters , $ , or _  </p>")
+//  document.write("<p> for example  $name , _name </p>")
+//  document.write ("<p> Variables names are case Sensitive </p>")
+//  document.write ("<p> Variable name should not be JS keywords") ;
+
+
+// chapter no 5 
+//  no 1 
+//  write a program that take two numbers and add them and its result print in your browers 
+
+// var first = 10;
+// var second = 45;
+// var result = first + second ;
+
+// document.write("sum of " + " " + first + " " + "and " + " " + second + " " + " is " + result);
+
+
+//  no 2 
+// Repeat question no 1 for subtraction , multiplication and division 
+
+
+//  subtraction 
+
+//  var first = 10;
+//  var second = 45;
+//  var result = first - second ;
+
+//  document.write("subtraction of " + " " + first + " " + "and " + " " + second + " " + " is " + result);
+
+
+
+
+// for multiplication 
+
+
+//  var first = 10;
+//  var second = 45;
+//  var result = first * second ;
+
+//  document.write("multiplication of  " + " " + first + " " + "and " + " " + second + " " + " is " + result);
+
+
+
+//  for division 
+
+
+//  var first = 10;
+//  var second = 45;
+//   var result = first / second ;
+
+//  document.write("division of  " + " " + first + " " + "and " + " " + second + " " + " is " + result);
+
+
+//  no 3 
+
+// var b ;
+
+// document.write("value of the b after declaration is " + b);
+
+// b = 13;
+// document.write(" <br> initial value :" + b);
+// b++;
+// document.write("<br> after increment is  " + b);
+// b = 7+ b;
+
+
+// document.write("<br> after addition is " + b );
+// b--;
+// document.write("<br> after decrement value is " + b);
+
+// b = b/3;
+// document.write("<br> the remainder is  :" + b);
+
+
+
+
+//  no 4 
+
+
+// var oneTicket = 600;
+// var totalPerson = 5;
+// var total = oneTicket * totalPerson;
+
+// document.write ("Total cost to buy" + " " + totalPerson + " " + " Tickets to a movies is " + " " + total);
+
+
+// no 5 
+
+//  prints the tables of 6 
+
+// var table = 6;
+// var one = table * 1 ;
+// var two = table * 2;
+// var three = table * 3;
+// var four = table * 4;
+// var five = table  * 5;
+// var six = table * 6;
+// var seven = table * 7;
+// var eight = table  * 8;
+// var nine = table  * 9;
+// var ten = table * 10;
+
+
+// document.write(" 6 " + " *" + " " + " 1" + " =" + one  );
+// document.write(  " <br> 6 " + " *" + " " + " 2" + " =" + two  )
+// document.write(" <br> 6 " + " *" + " " + " 3" + " =" + three  )
+// document.write("<br> 6 " + " *" + " " + " 4" + " =" + four  )
+// document.write(" <br> 6 " + " *" + " " + " 5" + " =" + five  )
+// document.write(" <br> 6 " + " *" + " " + " 6" + " =" + six  )
+// document.write(" <br> 6 " + " *" + " " + " 7" + " =" + seven  )
+// document.write(" <br> 6 " + " *" + " " + " 8" + " =" + eight  )
+// document.write(" <br> 6 " + " *" + " " + " 9" + " =" + nine  )
+// document.write("<br> 6 " + " *" + " " + " 10" + " =" + ten  )
+
+
+
+
+//  no  5 
+//  temperature converter
+// var c  = 3;
+// var f = 98;
+// var Celsius = ( f - 32)*5/9;
+// var Frenheite = ( c * 9/5) + 32;
+
+// document.write ( c + " celsus" +  " is " + Frenheite );
+// document.write("<br>" + f + " frenheite" + " is " + Celsius );
+
+
+//  no 6
+//  shopping cart 
+
+// var firtItem = 230;
+// var quantityFirstItem = 5;
+// var totalFirst = firtItem * quantityFirstItem
+// var secondItem = 180;
+// var quantitySecondItem = 7;
+// var totalSecond = secondItem * quantitySecondItem
+// var shippingCharges = 100;
+//  var totalPrice = totalFirst + totalSecond + shippingCharges;
+
+// document.write(" <h1> SHOPPING CART </h1> " );
+// document.write("<br> " + " price of the first item is " +  " " +  firtItem);
+// document.write("<br>" + " quantity of first item is " + "" + quantityFirstItem);
+// document.write("<br>" + " price of the second item is " + " " + secondItem );
+// document.write("<br>" + " quantity of second item is " + "" + quantitySecondItem);
+
+
+// document.write("<br>" + " shipping charges are " +  shippingCharges);
+
+// document.write("<br>" + " total cost of your order is " + " " + totalPrice  );
+
+
+//  no 7 
+//  marksheet 
+ 
+// var totalMarks = 980;
+// var obtain = 804;
+// var result = ( obtain * 100) / 980 ;
+
+// document.write("<h1> MARKSHEET </h1>");
+// document.write("<br>" + " Total marks :" + totalMarks);
+// document.write("<br>" + " obtained marks " + obtain);
+// document.write("<br>" + " total marks in percentage " + result);
